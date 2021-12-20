104567911bb2a62e6a5b81e0bd4dbf26c2dd359a
jiHHHHhhh <wlqudrkd@naver.com>
2021-12-20 21:12:58 +0900

    final

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        const call = async () => {
+         axios.get('/api/hello').then((res) => console.log(res.data));
+         };
+
+         useEffect(() => {
+            call();
+          }, []);
+
+         return (
+         <div className="App">
+         <header className="App-header">
+         <p>wpqkf</p>
+          </header>
+          </div>
+          );
 }
 
 export default App;
